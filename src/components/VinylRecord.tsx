82506ab1c437e0b3815149c1fd0e574634b45b54
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2023-09-23 10:31:43 +0200

    Update vinyl component

--- src/components/VinylRecord.tsx
+++ src/components/VinylRecord.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import classNames from "classnames";
+import Image from "next/image";
 import {
   Dispatch,
   ForwardedRef,
@@ -18,49 +20,25 @@ export const VinylRecord = forwardRef(function VinylRecordInner(
   { title, handleClick, isPlaying }: VinlyRecordProps,
   ref: ForwardedRef<HTMLButtonElement>
 ) {
-  const radii = [40, 35, 30, 25, 20];
   return (
     <button onClick={handleClick} className="relative w-full h-full" ref={ref}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 101 101"
-        width="100%"
-        height="100%"
-        className={`will-change-transform${
-          isPlaying ? " animate-spin-slow" : ""
-        }`}
-      >
-        <circle cx="51" cy="51" r="50" fill="black" />
-        <circle cx="51" cy="51" r="15" fill="green" />
-        <path
-          d="M38,51a13,13 0 1,0 26,0a13,13 0 1,0 -26,0"
-          id="label"
-          fill="transparent"
-        />
-        <circle cx="51" cy="51" r="2" fill="white" />
-        {radii.map((r) => (
-          <circle
-            cx="51"
-            cy="51"
-            r={r}
-            stroke="#222"
-            key={r}
-            fill="none"
-            strokeWidth={0.2}
-          />
-        ))}
-        <text
-          width="30"
-          fill="#fff"
-          fontSize={5}
-          fontWeight={300}
-          letterSpacing={0.5}
-          className="uppercase"
-        >
-          <textPath xlinkHref="#label">{title}</textPath>
-        </text>
-      </svg>
-      <div className="absolute rounded-full inset-0 bg-gradient-to-r mix-blend-screen opacity-20 from-black via-white to-black" />
+      <Image
+        src="/img/vinyl_static.png"
+        width={400}
+        height={400}
+        alt=""
+        className="absolute inset-0"
+      />
+      <Image
+        src="/img/vinyl_spinning.png"
+        width={400}
+        height={400}
+        alt=""
+        className={classNames(
+          "absolute inset-0",
+          isPlaying && "animate-spin-slow"
+        )}
+      />
     </button>
   );
 });
